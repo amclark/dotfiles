481fad56e543a83f3707655dddb51ac08b82d2eb
Andrew Clark <amclark7@gmail.com>
2013-07-09 09:31:52 -0700

    Slight modification to install script

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 # Very naive installation script, not safe, will not back anything up
+rm -rf ~/.vim_runtime
 cp -rf vimrc ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 cp -f dotfiles/.bashrc dotfiles/.bash_profile dotfiles/.bashrc_custom dotfiles/.tmux.conf ~
