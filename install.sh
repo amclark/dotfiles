e43d74caf17ca739b0e7c5e97a252a57ed95b2a4
Andrew Clark <amclark7@gmail.com>
2013-07-08 18:10:48 -0700

    Added tmux.conf install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # Very naive installation script, not safe, will not back anything up
 cp -rf vimrc ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
-cp -f dotfiles/.bashrc dotfiles/.bash_profile dotfiles/.bashrc_custom ~
+cp -f dotfiles/.bashrc dotfiles/.bash_profile dotfiles/.bashrc_custom dotfiles/.tmux.conf ~
 echo "Installed dotfiles!"
